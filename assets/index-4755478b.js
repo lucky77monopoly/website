72f6f8ed66a7184a300e307bf200402b597dc532
lucky77monopoly <152138904+lucky77monopoly@users.noreply.github.com>
2024-01-11 22:05:48 +0800

    Add files via upload

---
+++ assets/index-4755478b.js
@@ -0,0 +1,1 @@
+import{d as o,C as r,B as a}from"./arco-614d81ea.js";import{f as s,g as n}from"./vue-30aafe8a.js";const i=o({__name:"index",setup(c){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{i as default};
